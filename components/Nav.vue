41293867247a379adbb1ad08c0590669ad836112
Ronnie Villarini <Villarini91@gmail.com>
2020-07-16 15:27:43 -0500

    style active nav items

--- components/Nav.vue
+++ components/Nav.vue
@@ -4,30 +4,44 @@
     <div class="flex justify-end">
       <nuxt-link
         class="p-4 uppercase text-lg text-gray-700 hover:text-primary"
+        active-class="text-primary underline"
         exact
         to="/"
         >Home</nuxt-link
       >
       <nuxt-link
         class="p-4 uppercase text-lg text-gray-700 hover:text-primary"
+        active-class="text-primary underline"
         to="/portfolio"
         >Portfolio</nuxt-link
       >
       <nuxt-link
         class="p-4 uppercase text-lg text-gray-700 hover:text-primary"
+        active-class="text-primary underline"
         to="/about"
         >About</nuxt-link
       >
       <nuxt-link
         class="p-4 uppercase text-lg text-gray-700 hover:text-primary"
+        active-class="text-primary underline"
         to="/contact"
         >Contact</nuxt-link
       >
       <nuxt-link
         class="p-4 uppercase text-lg text-gray-700 hover:text-primary"
+        active-class="text-primary underline"
         to="/blog"
         >Blog</nuxt-link
       >
     </div>
   </nav>
 </template>
+
+<style scoped>
+.underline {
+  position: relative;
+  text-decoration-color: #6417e3;
+  text-decoration-thickness: 2px;
+  text-underline-offset: 0.3rem;
+}
+</style>
